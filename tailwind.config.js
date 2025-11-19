e51f4bb0896da6d3dd380e02624b7130da47411b
Lais-admin <adm@laisgalliz.net>
2025-11-19 13:28:01 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Onde o Tailwind deve procurar por classes
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      // Aqui você pode adicionar suas animações e cores personalizadas se precisar
+    },
+  },
+  plugins: [],
+}
