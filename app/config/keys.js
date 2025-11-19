3ccbe1621eaa0e70b64c1d7eaa138f3ea194f88b
Lais-admin <adm@laisgalliz.net>
2025-11-19 11:13:36 -0300

    Update keys.js

--- app/config/keys.js
+++ app/config/keys.js
@@ -1,7 +1,7 @@
 // LISTA DE CHAVES VÁLIDAS
 // Adicione aqui as chaves dos seus clientes quando vender
 export const validKeys = [
-  "VIP-GOLD-2025",
+  "VIP",
   "TESTE-ADMIN",
   "CLIENTE-MARIA-01"
 ];
